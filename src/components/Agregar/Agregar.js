75f3a6de5d51be8582ac4f3c2866005e16973e52
Moises Fuks <moshifuks2001@gmail.com>
2022-01-05 14:35:19 -0300

    firstCommit

--- src/components/Agregar/Agregar.js
+++ src/components/Agregar/Agregar.js
@@ -1,6 +1,24 @@
 import './Agregar.css'
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
 
 const Agregar = () => {
+    let producto = {}
+    let [nombre, setNombre] = useState("")
+    let [cantidad, setCantidad] = useState()
+    let [precio, setPrecio] = useState()
+
+    producto = {id: uuidv4(), nombre: nombre, cantidad: cantidad, precio: precio}
+
+    //Lo guardo en local Storage
+    const guardarDatos = (e) => {
+        e.preventDefault()
+        localStorage.setItem("producto", JSON.stringify(producto))
+        setNombre("")
+        setCantidad("")
+        setPrecio("")
+    }
+    
     return (
         <div className='container mt-4'>
             <h2>Agregar producto</h2>
@@ -8,17 +26,17 @@ const Agregar = () => {
             <form>
                 <div className="mb-3">
                     <label className="form-label">Nombre del producto</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control" onChange={(e) => setNombre(e.target.value)}/>
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Cantidad</label>
-                    <input type="number" className="form-control" />
+                    <input type="number" className="form-control" onChange={(e) => setCantidad(e.target.value)} />
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Precio</label>
-                    <input type="number" className="form-control" />
+                    <input type="number" className="form-control" onChange={(e) => setPrecio(e.target.value)}/>
                 </div>
-                <button type="submit" className="btn btn-success">Guardar</button>
+                <button type="submit" className="btn btn-success" onClick={guardarDatos}>Guardar</button>
             </form>
         </div>
     );
--- src/components/Productos/Productos.js
+++ src/components/Productos/Productos.js
@@ -1,8 +1,27 @@
 import { Link } from 'react-router-dom';
 import './Productos.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMinusCircle } from '@fortawesome/free-solid-svg-icons'
 
 const Agregar = () => {
+    let productos = []
+    let producto = {} 
+
+    //Obtengo mi producto del localStorage
+    producto = JSON.parse(localStorage.getItem('producto'))
+    
+    //Hago un push de producto a mis productos
+    /*productos.push(producto)
+    console.log(productos)*/
+
+    const eliminarItem = (id) => {
+        productos = productos.filter(producto =>(producto.id !== id
+        localStorage.removeItem(producto))
+    }
+    }
+
     return (
+
         <div className='container mt-4'>
             <h2>Productos</h2>
 
@@ -11,11 +30,33 @@ const Agregar = () => {
 
             <div className='row border-top border-bottom mt-4 p-2'>
                 <div className='col-2 fw-bolder'>#</div>
-                <div className='col-5 fw-bolder'>Nombre del producto</div>
+                <div className='col-5 fw-bolder'>Producto</div>
                 <div className='col-2 fw-bolder'>Cantidad</div>
                 <div className='col-2 fw-bolder'>Precio</div>
                 <div className='col-1 fw-bolder'>Acción</div>
             </div>
+            
+                {producto !== null?
+                    productos.push(producto)
+                :
+                    <p>No hay productos cargados</p>
+                }
+
+                {productos.length > 0?
+                    productos.map(prod => (
+                        <div className='row border-top border-bottom mt-4 p-2' key={prod.id}>
+                            <div className='col-2 fw-bolder'>#</div>
+                                <div className='col-5 fw-bolder'>{prod.nombre}</div>
+                                <div className='col-2 fw-bolder'>{prod.cantidad}</div>
+                                <div className='col-2 fw-bolder'>{prod.precio}</div>
+                                <div className='col-1 fw-bolder' onClick={() => eliminarItem(prod.id)}>
+                                    <FontAwesomeIcon icon={faMinusCircle} />
+                                </div>
+                            </div>
+                    ))
+                :
+                    <p>No hay productos cargados</p>
+                }
         </div>
     );
 }
